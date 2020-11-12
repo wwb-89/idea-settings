de5895320959130c5c53ce6b2186df4283238e48
zx <1964556591@qq.com>
2020-11-12 10:03:29 +0800

    创建编辑活动页面

--- activity-web/src/main/resources/static/pc/assets/js/create-active.js
+++ activity-web/src/main/resources/static/pc/assets/js/create-active.js
@@ -1,18 +1,4 @@
 
-//获取参数
- function getUrlParam(name){
-    var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
-    var r = window.location.search.substr(1).match(reg);
-    if (r!=null) return unescape(r[2]); return null;
-}
-let type = getUrlParam("type");//由哪个页面进入
-if(type == "create"){
-    $('#create').show();
-    $('#edit').hide();
-}else{
-    $('#edit').show();
-    $('#create').hide();
-}
 //上传图片
 function showImg(input) {
     var file = input.files[0];
@@ -30,52 +16,3 @@ function showImg1(input) {
     }
     reader.readAsDataURL(file)
 }
-
-$('#custom').click(function(){
-    $('.dailog-box1').fadeIn()
-    $('body,html').addClass('scroll');
-    $('.fenlei-input').val("自定义选项")
-})
-$('#addFenlei').click(function(){
-    $('.dailog-box1').fadeIn()
-    $('body,html').addClass('scroll');
-})
-$('.cancle').click(function(){
-    $('.dailog-box1').fadeOut();
-    $('.dailog-box2').fadeOut();
-    $('body,html').removeClass('scroll');
-    $('.fenlei-input').val("")
-})
-$('.close').click(function(){
-    $('.dailog-box1').fadeOut();
-    $('.dailog-box2').fadeOut();
-    $('body,html').removeClass('scroll');
-    $('.fenlei-input').val("")
-})
-$('#addModle').click(function(){
-    $('.dailog-box2').fadeIn();
-    $('body,html').addClass('scroll');
-})
-$('#next-step').click(function(){
-    $('.add-details').show();
-    $('.add-modle').hide();
-    $('#next-step').hide();
-    $('#complete').show();
-    $('body,html').addClass('scroll');
-})
-$('#complete').click(function(){
-    $('.dailog-box2').fadeOut();
-    $('body,html').removeClass('scroll');
-})
-$('.dellete').click(function(){
-    $(this).parent().hide()
-})
-
-
-// $('.dailog-box1').click(function(e){
-//     e.stopPropagation();
-//     e.preventDefault();
-//     $('.dailog-box1').hide();
-//     $('body,html').removeClass('scroll');
-// })
-// $('.active-name').on("input",$.fn.wordCount(20,$('.max')))
