517293b352b6add8be6a312c596cf209f52f37ab
wwb <wwb_89@outlook.com>
2020-12-10 19:33:32 +0800

    api支持跨域

--- activity-api/src/main/java/com/chaoxing/activity/api/mh/ActivityMhAppApiController.java
+++ activity-api/src/main/java/com/chaoxing/activity/api/mh/ActivityMhAppApiController.java
@@ -21,10 +21,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
@@ -46,6 +43,7 @@ import java.util.stream.Collectors;
  */
 @RestController
 @RequestMapping("mh")
+@CrossOrigin
 public class ActivityMhAppApiController {
 
 	/** 签到按钮地址 */
