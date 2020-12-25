fd7f71b366eb32dd65b25331d0ea438ddafe066a
wwb <wwb_89@outlook.com>
2020-12-25 14:54:31 +0800

    临时提交

---
+++ activity-admin/src/main/java/com/chaoxing/activity/admin/controller/ActivityManageController.java
@@ -0,0 +1,104 @@
+package com.chaoxing.activity.admin.controller;
+
+import com.chaoxing.activity.admin.util.LoginUtils;
+import com.chaoxing.activity.dto.LoginUserDTO;
+import com.chaoxing.activity.dto.activity.ActivityTypeDTO;
+import com.chaoxing.activity.dto.manager.WfwRegionalArchitectureDTO;
+import com.chaoxing.activity.dto.module.SignAddEditDTO;
+import com.chaoxing.activity.dto.sign.SignActivityManageIndexDTO;
+import com.chaoxing.activity.model.Activity;
+import com.chaoxing.activity.model.ActivityClassify;
+import com.chaoxing.activity.model.WebTemplate;
+import com.chaoxing.activity.service.WebTemplateService;
+import com.chaoxing.activity.service.activity.ActivityQueryService;
+import com.chaoxing.activity.service.activity.ActivityValidationService;
+import com.chaoxing.activity.service.activity.classify.ActivityClassifyQueryService;
+import com.chaoxing.activity.service.activity.scope.ActivityScopeQueryService;
+import com.chaoxing.activity.service.manager.module.SignApiService;
+import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+/**
+ * @author wwb
+ * @version ver 1.0
+ * @className AbsActivityManageController
+ * @description
+ * @blame wwb
+ * @date 2020-12-25 10:13:11
+ */
+@Component
+public class ActivityManageController {
+
+	@Resource
+	private ActivityQueryService activityQueryService;
+	@Resource
+	private ActivityClassifyQueryService activityClassifyQueryService;
+	@Resource
+	private SignApiService signApiService;
+	@Resource
+	private WebTemplateService webTemplateService;
+	@Resource
+	private ActivityScopeQueryService activityScopeQueryService;
+	@Resource
+	private ActivityValidationService activityValidationService;
+
+	public String index(String code) {
+		return "pc/activity-list";
+	}
+
+	public String activityIndex(Model model, @PathVariable Integer activityId, HttpServletRequest request) {
+		LoginUserDTO loginUser = LoginUtils.getLoginUser(request);
+		Activity activity = activityValidationService.manageAble(activityId, loginUser, null);
+		model.addAttribute("activity", activity);
+		SignActivityManageIndexDTO signActivityManageIndex = signApiService.statSignActivityManageIndex(activity.getSignId());
+		model.addAttribute("signActivityManageIndex", signActivityManageIndex);
+		return "pc/activity-index";
+	}
+
+	public String add(Model model, HttpServletRequest request) {
+		LoginUserDTO loginUser = LoginUtils.getLoginUser(request);
+		// 活动类型列表
+		List<ActivityTypeDTO> activityTypes = activityQueryService.listActivityType();
+		model.addAttribute("activityTypes", activityTypes);
+		// 活动分类列表
+		model.addAttribute("activityClassifies", activityClassifyQueryService.listOrgOptional(loginUser.getFid()));
+		// 报名签到
+		model.addAttribute("sign", SignAddEditDTO.builder().build());
+		// 模板列表
+		List<WebTemplate> webTemplates = webTemplateService.list();
+		model.addAttribute("webTemplates", webTemplates);
+		return "pc/activity-add-edit";
+	}
+
+	public String edit(Model model, @PathVariable Integer activityId, HttpServletRequest request, String code, Integer step) {
+		LoginUserDTO loginUser = LoginUtils.getLoginUser(request);
+		Activity activity = activityQueryService.getById(activityId);
+		model.addAttribute("activity", activity);
+		// 活动类型列表
+		model.addAttribute("activityTypes", activityQueryService.listActivityType());
+		// 活动分类列表
+		List<ActivityClassify> activityClassifies = activityClassifyQueryService.listOrgOptional(loginUser.getFid());
+		model.addAttribute("activityClassifies", activityClassifies);
+		// 报名签到
+		Integer signId = activity.getSignId();
+		SignAddEditDTO signAddEdit = null;
+		if (signId != null) {
+			signAddEdit = signApiService.getById(signId);
+		}
+		model.addAttribute("sign", signAddEdit);
+		// 模板列表
+		List<WebTemplate> webTemplates = webTemplateService.list();
+		model.addAttribute("webTemplates", webTemplates);
+		model.addAttribute("step", step);
+		// 活动参与范围
+		List<WfwRegionalArchitectureDTO> wfwRegionalArchitectures = activityScopeQueryService.listByActivityId(activityId);
+		model.addAttribute("participatedOrgs", wfwRegionalArchitectures);
+		return "pc/activity-add-edit";
+	}
+
+}
