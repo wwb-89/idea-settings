a782de1cf48283ce82b3e464ac2f38fca4cd9de4
wwb <wwb_89@outlook.com>
2020-12-30 11:21:33 +0800

    临时提交

--- activity-admin/src/main/java/com/chaoxing/activity/admin/interceptor/LoginRequiredInterceptor.java
+++ activity-admin/src/main/java/com/chaoxing/activity/admin/interceptor/LoginRequiredInterceptor.java
@@ -1,13 +1,20 @@
 package com.chaoxing.activity.admin.interceptor;
 
+import com.alibaba.fastjson.JSON;
 import com.chaoxing.activity.admin.util.LoginUtils;
 import com.chaoxing.activity.dto.LoginUserDTO;
+import com.chaoxing.activity.dto.RestRespDTO;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
@@ -26,17 +33,37 @@ public class LoginRequiredInterceptor extends HandlerInterceptorAdapter {
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 		LoginUserDTO loginUser = LoginUtils.getLoginUser(request);
 		if (loginUser == null) {
-			handleNotLogin(request, response);
+			if (!(handler instanceof HandlerMethod)) {
+				return true;
+			}
+			HandlerMethod method = (HandlerMethod) handler;
+			handleNotLogin(request, response, method);
 			return false;
 		}
 		return super.preHandle(request, response, handler);
 	}
 
-	private void handleNotLogin(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		// 编码refer的地址
-		String refer = request.getRequestURL().toString();
-		String encodedRefer = URLEncoder.encode(refer, StandardCharsets.UTF_8.name());
-		response.sendRedirect("http://v1.chaoxing.com/backSchool/toLogin?refer=" + encodedRefer);
+	private void handleNotLogin(HttpServletRequest request, HttpServletResponse response, HandlerMethod method) throws IOException {
+		String refer = request.getRequestURL().toString() + "?" + request.getQueryString();
+		refer = URLEncoder.encode(refer, StandardCharsets.UTF_8.name());
+		if (method == null) {
+			// 页面
+			response.sendRedirect("http://v1.chaoxing.com/backSchool/toLogin?refer=" + refer);
+		}else{
+			if (AnnotationUtils.findAnnotation(method.getMethod(), ResponseBody.class) != null
+					|| AnnotationUtils.findAnnotation(method.getBeanType(), ResponseBody.class) != null
+					|| AnnotationUtils.findAnnotation(method.getBeanType(), RestController.class) != null) {
+				// rest
+				response.setCharacterEncoding("utf-8");
+				response.setContentType("application/json; charset=utf-8");
+				PrintWriter writer = response.getWriter();
+				RestRespDTO restResDTO = RestRespDTO.error("请登录后操作");
+				writer.write(JSON.toJSONString(restResDTO));
+			} else {
+				// 页面
+				response.sendRedirect("http://v1.chaoxing.com/backSchool/toLogin?refer=" + refer);
+			}
+		}
 	}
 
 }
