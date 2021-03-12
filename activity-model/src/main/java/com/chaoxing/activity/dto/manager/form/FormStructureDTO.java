664e230c0ab8a48b21eb2e254ca9d1b1e90a6d89
wwb <wwb_89@outlook.com>
2021-03-12 16:05:34 +0800

    增加表单对象

---
+++ activity-model/src/main/java/com/chaoxing/activity/dto/manager/form/FormStructureDTO.java
@@ -0,0 +1,28 @@
+package com.chaoxing.activity.dto.manager.form;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**表单结构对象
+ * @author wwb
+ * @version ver 1.0
+ * @className FormStructureDTO
+ * @description
+ * @blame wwb
+ * @date 2021-03-09 12:28:12
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class FormStructureDTO {
+
+	/** 字段id */
+	private Integer id;
+	/** 字段名称 */
+	private String label;
+	private String compt;
+
+}
