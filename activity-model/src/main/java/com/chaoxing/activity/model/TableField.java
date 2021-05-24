4ec0eb56b71cd236d97df09742f629deb6940571
wwb <wwb_89@outlook.com>
2021-05-24 16:34:31 +0800

    表格字段配置枚举

--- activity-model/src/main/java/com/chaoxing/activity/model/TableField.java
+++ activity-model/src/main/java/com/chaoxing/activity/model/TableField.java
@@ -3,12 +3,10 @@ package com.chaoxing.activity.model;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 /**
  * 表格字段配置表
@@ -39,5 +37,61 @@ public class TableField {
     private LocalDateTime createTime;
     /** 更新时间; column: update_time*/
     private LocalDateTime updateTime;
+
+    @Getter
+    public enum Type {
+
+        /** 报名名单 */
+        SIGN_UP_LIST("报名名单", "sign_up_list"),
+        SIGN_IN_LIST("签到名单", "sign_in_list"),
+        ACTIVITY_STAT("活动统计", "activity_stat"),
+        USER_STAT("用户统计", "user_stat");
+
+        private final String name;
+        private final String value;
+
+        Type(String name, String value) {
+            this.name = name;
+            this.value = value;
+        }
+
+        public static Type fromValue(String value) {
+            Type[] values = Type.values();
+            for (Type type : values) {
+                if (Objects.equals(type.getValue(), value)) {
+                    return type;
+                }
+            }
+            return null;
+        }
+
+    }
+
+    @Getter
+    public enum AssociatedType {
+
+        /** 机构 */
+        ORG("机构", "org"),
+        ACTIVITY("活动", "activity");
+
+        private final String name;
+        private final String value;
+
+        AssociatedType(String name, String value) {
+            this.name = name;
+            this.value = value;
+        }
+
+        public static AssociatedType fromValue(String value) {
+            AssociatedType[] values = AssociatedType.values();
+            for (AssociatedType associatedType : values) {
+                if (Objects.equals(associatedType.getValue(), value)) {
+                    return associatedType;
+                }
+            }
+            return null;
+        }
+
+    }
 
 }
