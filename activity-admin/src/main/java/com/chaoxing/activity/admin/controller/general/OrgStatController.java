a7e3ad9a4732734b8184c2b818792637e28e0c91
wwb <wwb_89@outlook.com>
2021-05-27 18:45:09 +0800

    复杂表格ztree样式问题

--- activity-admin/src/main/java/com/chaoxing/activity/admin/controller/general/OrgStatController.java
+++ activity-admin/src/main/java/com/chaoxing/activity/admin/controller/general/OrgStatController.java
@@ -1,5 +1,10 @@
 package com.chaoxing.activity.admin.controller.general;
 
+import com.chaoxing.activity.admin.util.LoginUtils;
+import com.chaoxing.activity.dto.LoginUserDTO;
+import com.chaoxing.activity.model.OrgTableField;
+import com.chaoxing.activity.model.TableField;
+import com.chaoxing.activity.model.TableFieldDetail;
 import com.chaoxing.activity.service.tablefield.TableFieldQueryService;
 import com.chaoxing.activity.util.annotation.LoginRequired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 import java.util.Optional;
 
 /**机构统计
@@ -41,7 +47,14 @@ public class OrgStatController {
     @RequestMapping("user")
     public String index(HttpServletRequest request, Model model, Integer wfwfid, Integer unitId, Integer state, Integer fid) {
         Integer realFid = Optional.ofNullable(wfwfid).orElse(Optional.ofNullable(unitId).orElse(Optional.ofNullable(state).orElse(fid)));
-//        tableFieldQueryService.listOrgTableField(realFid, )
+        if (realFid == null) {
+            LoginUserDTO loginUser = LoginUtils.getLoginUser(request);
+            realFid = loginUser.getFid();
+        }
+        List<TableFieldDetail> tableFieldDetails = tableFieldQueryService.listTableFieldDetail(TableField.Type.USER_STAT, TableField.AssociatedType.ORG);
+        model.addAttribute("tableFieldDetails", tableFieldDetails);
+        List<OrgTableField> orgTableFields = tableFieldQueryService.listOrgTableField(realFid, TableField.Type.USER_STAT, TableField.AssociatedType.ORG);
+        model.addAttribute("orgTableFields", orgTableFields);
         return "pc/stat/org-user-stat";
     }
 
