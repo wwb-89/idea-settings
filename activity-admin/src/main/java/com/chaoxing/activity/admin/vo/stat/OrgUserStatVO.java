83a965e22ec6ba002b06b7092a85b350682c3bac
wwb <wwb_89@outlook.com>
2021-05-28 10:37:05 +0800

    添加vo层

---
+++ activity-admin/src/main/java/com/chaoxing/activity/admin/vo/stat/OrgUserStatVO.java
@@ -0,0 +1,12 @@
+package com.chaoxing.activity.admin.vo.stat;
+
+/**
+ * @author wwb
+ * @version ver 1.0
+ * @className OrgUserStatVO
+ * @description
+ * @blame wwb
+ * @date 2021-05-28 10:36:45
+ */
+public class OrgUserStatVO {
+}
