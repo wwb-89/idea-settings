b56044f65f5cf4337410a7552ab6f714b02577a0
wwb <wwb_89@outlook.com>
2021-06-09 17:56:43 +0800

    sql更新

--- sql/待执行.sql
+++ sql/待执行.sql
@@ -1,3 +1,8 @@
 -- 2021-06-09
 ALTER TABLE `t_activity` ADD COLUMN is_timing_release TINYINT ( 1 ) DEFAULT 0 COMMENT '是否定时发布';
 ALTER TABLE `t_activity` ADD COLUMN timing_release_time datetime COMMENT '定时发布时间';
+UPDATE t_activity t
+SET t.is_timing_release = 0,
+    t.timing_release_time = NULL
+WHERE
+    ISNULL(t.is_timing_release);
