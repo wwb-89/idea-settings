247b54bec6f181744b17b585990de1b74da53a51
wwb <wwb_89@outlook.com>
2021-06-11 12:32:41 +0800

    Mapper注解引用错误

--- activity-mapper/src/main/java/com/chaoxing/activity/mapper/UserDataPushRecordMapper.java
+++ activity-mapper/src/main/java/com/chaoxing/activity/mapper/UserDataPushRecordMapper.java
@@ -2,7 +2,7 @@ package com.chaoxing.activity.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.chaoxing.activity.model.UserDataPushRecord;
-import org.mapstruct.Mapper;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @className: UserDataPushRecordMapper
