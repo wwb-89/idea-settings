bd71af92a62c2ceca503aba0ee7b0d54ebe4d6a3
wwb <wwb_89@outlook.com>
2021-07-07 10:11:41 +0800

    报名表单填写类型

--- activity-model/src/main/java/com/chaoxing/activity/model/SignUpFillInfoType.java
+++ activity-model/src/main/java/com/chaoxing/activity/model/SignUpFillInfoType.java
@@ -3,10 +3,9 @@ package com.chaoxing.activity.model;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+
+import java.util.Objects;
 
 /**
  * 报名填报信息类型表：默认、双选会、微服务表单
@@ -31,6 +30,32 @@ public class SignUpFillInfoType {
     /** 类型; column: type*/
     private String type;
 
+    @Getter
+    public enum TypeEnum {
 
+        /** 普通表单 */
+        FORM("普通表单", "form"),
+        DUAL_SELECT_FORM("双选会表单", "dual_select_form"),
+        WFW_FORM("微服务表单", "wfw_form");
+
+        private String name;
+        private String value;
+
+        TypeEnum(String name, String value) {
+            this.name = name;
+            this.value = value;
+        }
+
+        public static TypeEnum fromValue(String value) {
+            TypeEnum[] values = TypeEnum.values();
+            for (TypeEnum typeEnum : values) {
+                if (Objects.equals(typeEnum.getValue(), value)) {
+                    return typeEnum;
+                }
+            }
+            return null;
+        }
+
+    }
 
 }
