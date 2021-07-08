6d638c8b9ef80128d2e2a3279bd79656e4b64e4c
huxiaolong <13923154647@163.com>
2021-07-08 18:06:38 +0800

    引擎设计查询接口

--- activity-admin/src/main/java/com/chaoxing/activity/admin/controller/api/ActivityEngineApiController.java
+++ activity-admin/src/main/java/com/chaoxing/activity/admin/controller/api/ActivityEngineApiController.java
@@ -1,6 +1,9 @@
 package com.chaoxing.activity.admin.controller.api;
 
+import com.alibaba.fastjson.JSON;
 import com.chaoxing.activity.dto.RestRespDTO;
+import com.chaoxing.activity.dto.engine.ActivityEngineDTO;
+import com.chaoxing.activity.service.activity.engine.ActivityEngineHandleService;
 import com.chaoxing.activity.service.activity.engine.ActivityEngineQueryService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,9 +25,34 @@ public class ActivityEngineApiController {
     @Resource
     private ActivityEngineQueryService activityEngineQueryService;
 
-    @PostMapping("design")
-    public RestRespDTO index(Integer fid, Integer templateId) {
+    @Resource
+    private ActivityEngineHandleService activityEngineHandleService;
 
+
+    /**获取模板详细信息(模板信息、模板组件、组件列表)
+    * @Description
+    * @author huxiaolong
+    * @Date 2021-07-08 17:24:12
+    * @param fid
+    * @param templateId
+    * @return com.chaoxing.activity.dto.RestRespDTO
+    */
+    @PostMapping("info")
+    public RestRespDTO listOrgComponent(Integer fid, Integer templateId) {
+        return RestRespDTO.success(activityEngineQueryService.findEngineTemplateInfo(fid, templateId));
+    }
+
+    /**发布模板(新增/修改)
+    * @Description
+    * @author huxiaolong
+    * @Date 2021-07-08 17:24:09
+    * @param templateInfoStr
+    * @return com.chaoxing.activity.dto.RestRespDTO
+    */
+    @PostMapping("publish")
+    public RestRespDTO publish(String templateInfoStr) {
+        ActivityEngineDTO activityEngineDTO = JSON.parseObject(templateInfoStr, ActivityEngineDTO.class);
+        activityEngineHandleService.handleEngineTemplate(activityEngineDTO);
         return RestRespDTO.success();
     }
 }
--- activity-service/src/main/java/com/chaoxing/activity/service/activity/engine/ActivityEngineQueryService.java
+++ activity-service/src/main/java/com/chaoxing/activity/service/activity/engine/ActivityEngineQueryService.java
@@ -1,12 +1,16 @@
 package com.chaoxing.activity.service.activity.engine;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.chaoxing.activity.dto.engine.ActivityEngineDTO;
 import com.chaoxing.activity.mapper.ComponentFieldMapper;
 import com.chaoxing.activity.mapper.ComponentMapper;
 import com.chaoxing.activity.mapper.TemplateComponentMapper;
 import com.chaoxing.activity.mapper.TemplateMapper;
+import com.chaoxing.activity.model.Component;
 import com.chaoxing.activity.model.Template;
+import com.chaoxing.activity.model.TemplateComponent;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -33,8 +37,24 @@ public class ActivityEngineQueryService {
     private ComponentFieldMapper componentFieldMapper;
 
 
+    public ActivityEngineDTO findEngineTemplateInfo(Integer fid, Integer templateId) {
+        // 查询模板数据
+        Template template = templateMapper.selectById(templateId);
+        // 查询组件数据
+        List<Component> components = listComponentByFid(fid);
+        // 查询模板组件关联关系
+        List<TemplateComponent> templateComponents = listTemplateComponentByTemplateId(templateId);
+
+        return ActivityEngineDTO.builder()
+                .template(template)
+                .components(components)
+                .templateComponents(templateComponents)
+                .build();
+    }
+
+
     /**根据机构fid，查询除系统模板外，其他模板
-    * @Description 
+    * @Description
     * @author huxiaolong
     * @Date 2021-07-06 14:35:58
     * @param fid
@@ -49,5 +69,41 @@ public class ActivityEngineQueryService {
                 .orderByAsc(Template::getSequence));
     }
 
+    /**
+    * @Description
+    * @author huxiaolong
+    * @Date 2021-07-07 15:31:05
+    * @param fid
+    * @return void
+    */
+    public List<Component> listComponentByFid(Integer fid) {
+        // 系统组件(isSystem: true, fid: null) + fid 自身的组件
+        List<Component> components = componentMapper.selectList(new QueryWrapper<Component>()
+                .lambda()
+                .eq(Component::getFid, fid)
+                .or(j -> j.eq(Component::getSystem, Boolean.TRUE)
+                        .isNull(Component::getFid)));
 
+        // todo 自定义组件处理
+        for (Component component : components) {
+            if (!component.getSystem() && StringUtils.isNotBlank(component.getType())) {
+
+            }
+        }
+        return components;
+    }
+
+    /**
+    * @Description
+    * @author huxiaolong
+    * @Date 2021-07-07 15:30:53
+    * @param templateId
+    * @return void
+    */
+    public List<TemplateComponent> listTemplateComponentByTemplateId(Integer templateId) {
+        return templateComponentMapper.selectList(new QueryWrapper<TemplateComponent>()
+                .lambda()
+                .eq(TemplateComponent::getTemplateId, templateId)
+                .orderByAsc(TemplateComponent::getSequence));
+    }
 }
