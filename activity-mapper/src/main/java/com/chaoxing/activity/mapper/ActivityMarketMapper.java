63126e5a2d58ac09021bc315fcab02c585574e9e
wwb <wwb_89@outlook.com>
2021-08-09 19:20:31 +0800

    添加活动可以发布到多个活动市场的实体支持

---
+++ activity-mapper/src/main/java/com/chaoxing/activity/mapper/ActivityMarketMapper.java
@@ -0,0 +1,16 @@
+package com.chaoxing.activity.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.chaoxing.activity.model.ActivityMarket;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @className: ActivityMarketMapper
+ * @Description: 
+ * @author: mybatis generator
+ * @date: 2021-08-09 19:17:42
+ * @version: ver 1.0
+ */
+@Mapper
+public interface ActivityMarketMapper extends BaseMapper<ActivityMarket> {
+}
