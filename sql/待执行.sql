a6ca48144e934659d40d22d6b95153d6d454a601
wwb <wwb_89@outlook.com>
2021-08-10 10:00:54 +0800

    活动表删除是否置顶字段

--- sql/待执行.sql
+++ sql/待执行.sql
@@ -1,13 +1,2 @@
-ALTER TABLE t_activity ADD is_top TINYINT(1) DEFAULT 0 COMMENT '是否置顶';
-CREATE TABLE `t_activity_market`  (
-    `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
-    `activity_id` int(11) NULL COMMENT '活动id',
-    `market_id` int(11) NULL COMMENT '市场id',
-    `status` int(11) NULL COMMENT '活动状态',
-    `is_released` tinyint(1) NULL DEFAULT 0 COMMENT '是否发布',
-    `is_top` tinyint(1) NULL DEFAULT 0 COMMENT '是否置顶',
-    PRIMARY KEY (`id`),
-    INDEX `idx_primary`(`activity_id`, `market_id`),
-    INDEX `idx_market_id`(`market_id`)
-) COMMENT = '活动与活动市场关联表';
+
 
