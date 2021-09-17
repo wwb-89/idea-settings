e7c02743c50971823538a18f136e6a06952c1c77
huxiaolong <13923154647@163.com>
2021-09-17 15:06:59 +0800

    新增活动标识与区域code关联表功能逻辑

---
+++ activity-mapper/src/main/java/com/chaoxing/activity/mapper/ActivityFlagCodeMapper.java
@@ -0,0 +1,19 @@
+package com.chaoxing.activity.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.chaoxing.activity.model.ActivityFlagCode;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * @className: ActivityFlagCodeMapper
+ * @Description: 
+ * @author: mybatis generator
+ * @date: 2021-09-17 10:50:23
+ * @version: ver 1.0
+ */
+@Mapper
+public interface ActivityFlagCodeMapper extends BaseMapper<ActivityFlagCode> {
+    
+}
