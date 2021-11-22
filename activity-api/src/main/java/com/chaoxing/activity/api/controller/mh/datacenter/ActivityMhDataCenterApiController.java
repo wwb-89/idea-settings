3daddb229adcd384186c16af4f938076b9011cfb
wwb <wwb_89@outlook.com>
2021-11-22 11:05:41 +0800

    大数据积分域名问题

--- activity-api/src/main/java/com/chaoxing/activity/api/controller/mh/datacenter/ActivityMhDataCenterApiController.java
+++ activity-api/src/main/java/com/chaoxing/activity/api/controller/mh/datacenter/ActivityMhDataCenterApiController.java
@@ -267,7 +267,7 @@ public class ActivityMhDataCenterApiController {
             fields.add(buildField("发布时间", Optional.ofNullable(record.getReleaseTime()).map(DateTimeFormatterConstant.YYYY_MM_DD_HH_MM::format).orElse(""), ++fieldFlag));
             fields.add(buildField("typeID", record.getActivityClassifyId(), 102));
             fields.add(buildField("活动时间段", DateUtils.activityTimeScope(record.getStartTime(), record.getEndTime(), DateUtils.MIDDLE_LINE_SEPARATOR), 102));
-            Boolean needStatusValue = urlParams.getBoolean("needStatusValue");
+            Boolean needStatusValue = Optional.ofNullable(urlParams.getBoolean("needStatusValue")).orElse(false);
             if (needStatusValue) {
                 fields.add(buildField("活动状态value", getStatusValue(record), ++fieldFlag));
             }
