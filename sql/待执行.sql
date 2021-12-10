c74ba728bc9f2a896a763ab23ef290ff7e7a3b43
huxiaolong <13923154647@163.com>
2021-12-10 12:53:50 +0800

    更新报名信息填报sql

---
+++ sql/待执行.sql
@@ -0,0 +1,9 @@
+-- 更新新模板字段排序，每个排序值增大五倍，以便后插入字段能够排序在其中间
+UPDATE t_template_component set `sequence` = `sequence` * 5 where template_id in (13, 14, 15) and `sequence` != -1;
+UPDATE t_template_component set `sequence` = 46 where template_id in (13, 14, 15) and component_id = 48;
+
+
+-- 补充系统模板遗漏的报名信息填报模板关联数据
+INSERT INTO `activity_engine`.`t_sign_up_fill_info_type` (`id`, `template_component_id`, `type`, `wfw_form_template_id`) VALUES (12, 236, 'wfw_form', 5);
+INSERT INTO `activity_engine`.`t_sign_up_fill_info_type` (`id`, `template_component_id`, `type`, `wfw_form_template_id`) VALUES (13, 257, 'wfw_form', 5);
+INSERT INTO `activity_engine`.`t_sign_up_fill_info_type` (`id`, `template_component_id`, `type`, `wfw_form_template_id`) VALUES (14, 278, 'wfw_form', 5);
