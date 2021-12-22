351c81c36f9e3465aacd472610e31659d937f5ab
wwb <wwb_89@outlook.com>
2021-12-22 17:57:49 +0800

    已执行sql迁移

--- sql/已执行/2021-12.sql
+++ sql/已执行/2021-12.sql
@@ -181,3 +181,18 @@ INSERT INTO `activity_engine`.`t_table_field_detail`(`id`, `table_field_id`, `na
 INSERT INTO `activity_engine`.`t_table_field_detail`(`id`, `table_field_id`, `name`, `code`, `is_default_checked`, `is_allow_uncheck`, `is_default_top`, `is_allow_top`, `is_sortable`, `min_width`, `align`, `sequence`, `is_deleted`, `create_time`, `update_time`) VALUES (57, 5, '合格状态', 'qualifiedStatus', 0, 1, 0, 0, 0, 50, 'left', 40, 0, '2021-12-20 10:46:53', '2021-12-20 10:50:30');
 -- 2021-12-21
 INSERT INTO `activity_engine`.`t_table_field_detail`(`id`, `table_field_id`, `name`, `code`, `is_default_checked`, `is_allow_uncheck`, `is_default_top`, `is_allow_top`, `is_sortable`, `min_width`, `align`, `sequence`, `is_deleted`, `create_time`, `update_time`) VALUES (58, 3, '参与时长(分钟)', 'participateTimeLength', 0, 1, 0, 1, 1, 120, 'left', 30, 0, '2021-12-21 15:33:19', '2021-12-21 15:36:43');
+-- 2021-12-22
+-- 增加活动推送提醒组件
+INSERT INTO `t_component` (`id`, `pid`, `name`, `code`, `is_required`, `introduction`, `is_system`, `is_multi`, `type`, `data_origin`, `origin_identify`, `field_flag`, `template_id`, `create_time`, `create_uid`, `update_time`, `update_uid`) VALUES (50, 0, '推送提醒', 'push_reminder', 0, '开启后，可为固定人群发送活动推送', 1, 0, NULL, NULL, NULL, NULL, NULL, '2021-12-21 15:14:20', 172649568, '2021-12-21 15:14:20', 172649568);
+-- 活动增加是否开启推送提醒字段
+ALTER TABLE t_activity ADD is_open_push_reminder TINYINT(1) DEFAULT 0 COMMENT '是否开启推送提醒';
+-- 创建活动提醒推送表
+CREATE TABLE `t_activity_push_reminder` (
+    `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+    `activity_id` int(11) DEFAULT NULL COMMENT '活动id',
+    `receive_scope` text COMMENT '推送提醒接收范围',
+    `content` varchar(255) DEFAULT NULL COMMENT '通知内容',
+    `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
