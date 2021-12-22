8fffd159fc9fef211e0a8013f6f4f912a16db12e
wwb <wwb_89@outlook.com>
2021-12-22 10:21:17 +0800

    已执行sql迁移

--- sql/待执行.sql
+++
@@ -1,2 +0,0 @@
--- 2021-12-21
-INSERT INTO `activity_engine`.`t_table_field_detail`(`id`, `table_field_id`, `name`, `code`, `is_default_checked`, `is_allow_uncheck`, `is_default_top`, `is_allow_top`, `is_sortable`, `min_width`, `align`, `sequence`, `is_deleted`, `create_time`, `update_time`) VALUES (58, 3, '参与时长(分钟)', 'participateTimeLength', 0, 1, 0, 1, 1, 120, 'left', 30, 0, '2021-12-21 15:33:19', '2021-12-21 15:36:43');
