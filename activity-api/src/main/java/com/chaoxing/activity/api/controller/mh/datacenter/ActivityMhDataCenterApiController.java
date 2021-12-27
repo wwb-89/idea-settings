263f009656f3d8adab8482beb385118cd8809863
wwb <wwb_89@outlook.com>
2021-12-27 15:19:23 +0800

    封装活动数据为门户标准接收格式种分类id和时间的flag冲突的bug

--- activity-api/src/main/java/com/chaoxing/activity/api/controller/mh/datacenter/ActivityMhDataCenterApiController.java
+++ activity-api/src/main/java/com/chaoxing/activity/api/controller/mh/datacenter/ActivityMhDataCenterApiController.java
@@ -285,7 +285,7 @@ public class ActivityMhDataCenterApiController {
             fields.add(buildField("活动标识", record.getActivityFlag(), ++fieldFlag));
             fields.add(buildField("发布时间", Optional.ofNullable(record.getReleaseTime()).map(DateTimeFormatterConstant.YYYY_MM_DD_HH_MM::format).orElse(""), ++fieldFlag));
             fields.add(buildField("typeID", record.getActivityClassifyId(), 102));
-            fields.add(buildField("活动时间段", DateUtils.activityTimeScope(record.getStartTime(), record.getEndTime(), DateUtils.MIDDLE_LINE_SEPARATOR), 102));
+            fields.add(buildField("活动时间段", DateUtils.activityTimeScope(record.getStartTime(), record.getEndTime(), DateUtils.MIDDLE_LINE_SEPARATOR), 103));
             Boolean needStatusValue = Optional.ofNullable(urlParams.getBoolean("needStatusValue")).orElse(false);
             if (needStatusValue) {
                 fields.add(buildField("活动状态value", getStatusValue(record), ++fieldFlag));
