338e2caa368039736dc196e6db54e5f2ca490cf1
huxiaolong <13923154647@163.com>
2021-12-31 16:59:19 +0800

    修复

--- activity-api/src/main/java/com/chaoxing/activity/task/config/SchedulingConfig.java
+++ activity-api/src/main/java/com/chaoxing/activity/task/config/SchedulingConfig.java
@@ -1,6 +1,9 @@
 package com.chaoxing.activity.task.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 /**
@@ -11,11 +14,11 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
  * @blame wwb
  * @date 2021-02-03 19:36:00
  */
-//@Configuration
-//@EnableScheduling
+@Configuration
+@EnableScheduling
 public class SchedulingConfig {
 
-//	@Bean
+	@Bean
 	public TaskScheduler taskScheduler() {
 		ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
 		taskScheduler.setPoolSize(100);
