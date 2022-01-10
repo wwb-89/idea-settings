b70fe7d2d23c4a27cdd9140c5e5fca6fcb244468
huxiaolong <13923154647@163.com>
2022-01-10 18:08:50 +0800

    学年计算方法bug处理

--- activity-util/src/main/java/com/chaoxing/activity/util/SchoolYearSemesterUtils.java
+++ activity-util/src/main/java/com/chaoxing/activity/util/SchoolYearSemesterUtils.java
@@ -12,6 +12,10 @@ import java.time.LocalDateTime;
  */
 public class SchoolYearSemesterUtils {
 
+	/** 一年中的第一个月 */
+	private static final int FIRST_MONTH = 1;
+	/** 一年中的最后一个月 */
+	private static final int LAST_MONTH = 12;
 	/** 上学期分割月 */
 	private static final int LAST_SEMESTER_SPLIT_MONTH = 3;
 	/** 下学期分割月 */
@@ -44,12 +48,16 @@ public class SchoolYearSemesterUtils {
 		int year = current.getYear();
 		int monthValue = current.getMonthValue();
 		int dayOfMonth = current.getDayOfMonth();
-
-		if (monthValue >= LAST_SEMESTER_SPLIT_MONTH) {
-			// 上学期
+		// 1 - 9 月， 属于上一学年，当前年-1
+		if (monthValue < NEXT_SEMESTER_SPLIT_MONTH) {
+			year -= 1;
+		}
+		// 3 - 9 月属于下一学期
+		if (monthValue >= LAST_SEMESTER_SPLIT_MONTH && monthValue < NEXT_SEMESTER_SPLIT_MONTH) {
+			// 下学期
 			return LocalDateTime.of(year, LAST_SEMESTER_SPLIT_MONTH, SEMESTER_SPLIT_DAY, INIT_ZERO, INIT_ZERO, INIT_ZERO);
 		} else {
-			// 下学期
+			// 上学期
 			return LocalDateTime.of(year, NEXT_SEMESTER_SPLIT_MONTH, SEMESTER_SPLIT_DAY, INIT_ZERO, INIT_ZERO, INIT_ZERO);
 		}
 	}
