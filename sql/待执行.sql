0229b7becdfe628fbf1e9c072704ffd8bea91cfd
wwb <wwb_89@outlook.com>
2022-01-19 15:38:52 +0800

    已执行sql迁移

--- sql/待执行.sql
+++ sql/待执行.sql
@@ -1,11 +1,2 @@
-UPDATE t_user_stat_summary t,
-    t_activity t1
-SET t.period = t1.period,
-    t.credit = t1.credit
-WHERE
-    t.activity_id = t1.id;
-
--- 2022-01-18
-INSERT INTO `activity_engine`.`t_component` (`id`, `pid`, `name`, `code`, `is_required`, `introduction`, `is_system`, `is_multi`, `type`, `data_origin`, `origin_identify`, `field_flag`, `template_id`, `create_time`, `create_uid`, `update_time`, `update_uid`) VALUES (52, 13, '角色范围', 'sign_up_role_limit', 0, '开启后，报名者需要是对应角色才能报名', 1, 0, NULL, NULL, NULL, NULL, NULL, '2022-01-18 10:48:54', 25418810, '2022-01-18 10:50:35', 25418810);
-
-
+-- 2022-01-19
+ALTER TABLE t_activity_stat_summary ADD pv INTEGER DEFAULT 0 COMMENT 'pv';
