1d2f3cffda92ccfde68306e77766331cc48c4cdd
huxiaolong <13923154647@163.com>
2022-01-21 11:20:03 +0800

    完善菜单删除描述，确认组件增加reverse调置确认删除按钮

--- activity-admin/src/main/resources/static/pc/assets/component/confirm-common.js
+++ activity-admin/src/main/resources/static/pc/assets/component/confirm-common.js
@@ -1,10 +1,14 @@
 Vue.component('vue-confirm-common', {
-    props: ['message', 'sure', 'cancel'],
+    props: ['message', 'sure', 'cancel', 'reverse'],
     template: "<div class='dialog-box' v-show='show'>\n" +
         "    <div class='confirm_dialog'>\n" +
         "        <span class='icon'><img :src='warnImgUrl' class='warn'></span>\n" +
         "        <span class='text'>{{message}}</span>\n" +
-        "        <div class='btn'>\n" +
+        "        <div class='btn' v-if='reverse'>\n" +
+        "            <span class='ok' style='margin-right: 24px' @click='sureCallback'>{{sure}}</span>\n" +
+        "            <span class='cancel' style='margin-right: 0' @click='show = false'>{{cancel}}</span>\n" +
+        "        </div>\n" +
+        "        <div class='btn' v-else>\n" +
         "            <span class='cancel' @click='show = false'>{{cancel}}</span>\n" +
         "            <span class='ok' @click='sureCallback'>{{sure}}</span>\n" +
         "        </div>\n" +
