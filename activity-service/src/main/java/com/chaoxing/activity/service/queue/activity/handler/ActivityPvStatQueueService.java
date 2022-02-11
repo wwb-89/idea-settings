31db560acc9aa86b7bbe0e193924bf970ebf34b8
wwb <wwb_89@outlook.com>
2022-02-11 14:32:12 +0800

    湖北群艺馆1413市场下的活动每天统计访问量，并更新到万能表单中

--- activity-service/src/main/java/com/chaoxing/activity/service/queue/activity/handler/ActivityPvStatQueueService.java
+++ activity-service/src/main/java/com/chaoxing/activity/service/queue/activity/handler/ActivityPvStatQueueService.java
@@ -1,9 +1,13 @@
 package com.chaoxing.activity.service.queue.activity.handler;
 
+import com.chaoxing.activity.model.Activity;
+import com.chaoxing.activity.service.activity.ActivityQueryService;
 import com.chaoxing.activity.service.activity.stat.ActivityStatSummaryHandlerService;
 import com.chaoxing.activity.service.manager.MhApiService;
 import com.chaoxing.activity.service.queue.activity.ActivityPvStatQueue;
+import com.chaoxing.activity.service.queue.activity.WfwFormActivityDataUpdateQueue;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -22,9 +26,14 @@ import java.util.Optional;
 public class ActivityPvStatQueueService {
 
 	@Resource
+	private ActivityQueryService activityQueryService;
+	@Resource
 	private MhApiService mhApiService;
 	@Resource
 	private ActivityStatSummaryHandlerService activityStatSummaryHandlerService;
+	@Resource
+	private WfwFormActivityDataUpdateQueue wfwFormActivityDataUpdateQueue;
+
 
 	public void handle(ActivityPvStatQueue.QueueParamDTO queueParam) {
 		if (queueParam == null) {
@@ -36,7 +45,26 @@ public class ActivityPvStatQueueService {
 		}
 		Integer pv = mhApiService.countWebsitePv(websiteId);
 		pv = Optional.ofNullable(pv).orElse(0);
-		activityStatSummaryHandlerService.updatePv(queueParam.getActivityId(), pv);
+		Integer activityId = queueParam.getActivityId();
+		activityStatSummaryHandlerService.updatePv(activityId, pv);
+		// pv更改触发活动信息更新（万能表单创建的活动）
+		Activity activity = activityQueryService.getById(activityId);
+		if (activity == null) {
+			return;
+		}
+		// 万能表单活动数据更新
+		if (activity.isWfwFormActivity()) {
+			Integer formId = Optional.ofNullable(activity.getOrigin()).filter(StringUtils::isNotBlank).map(Integer::parseInt).orElse(null);
+			Integer formUserId = activity.getOriginFormUserId();
+			if (formId != null && formUserId != null) {
+				wfwFormActivityDataUpdateQueue.push(WfwFormActivityDataUpdateQueue.QueueParamDTO.builder()
+						.activityId(activityId)
+						.fid(activity.getCreateFid())
+						.formId(formId)
+						.formUserId(activity.getOriginFormUserId())
+						.build());
+			}
+		}
 	}
 
 }
