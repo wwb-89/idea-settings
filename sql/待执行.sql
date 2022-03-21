df1ad151ea27f800e9fccea6db55249442c2096f
wwb <wwb_89@outlook.com>
2022-03-21 18:59:44 +0800

    解决ztree的level与区域架构的level冲突的问题

--- sql/待执行.sql
+++
@@ -1,5 +0,0 @@
--- 2022-03-21
-ALTER TABLE t_template_component ADD is_not_match_show_tips TINYINT DEFAULT 0 COMMENT '不满足条件时是否提示文字';
-ALTER TABLE t_template_component ADD not_match_tips varchar(255) DEFAULT "" COMMENT '不满足条件时的提示文字';
-ALTER TABLE t_template_component ADD is_not_match_jump TINYINT DEFAULT 0 COMMENT '不满足条件时是否点击跳转';
-ALTER TABLE t_template_component ADD not_match_jump_url varchar(255) DEFAULT "" COMMENT '不满足条件时跳转链接';
