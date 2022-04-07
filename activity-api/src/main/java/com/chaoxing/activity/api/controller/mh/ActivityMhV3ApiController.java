7ac42db2dbd4e5ee43514e1101a369ffb91ed4ed
wwb <wwb_89@outlook.com>
2022-04-07 19:15:55 +0800

    新接口替换旧接口

--- activity-api/src/main/java/com/chaoxing/activity/api/controller/mh/ActivityMhV3ApiController.java
+++ activity-api/src/main/java/com/chaoxing/activity/api/controller/mh/ActivityMhV3ApiController.java
@@ -24,6 +24,7 @@ import com.chaoxing.activity.service.manager.CloudApiService;
 import com.chaoxing.activity.service.manager.module.SignApiService;
 import com.chaoxing.activity.service.manager.module.WorkApiService;
 import com.chaoxing.activity.service.util.MhDataBuildUtil;
+import com.chaoxing.activity.util.ActivityStatusUtils;
 import com.chaoxing.activity.util.DateUtils;
 import com.chaoxing.activity.util.constant.DateTimeFormatterConstant;
 import com.chaoxing.activity.util.constant.DomainConstant;
@@ -37,6 +38,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -79,7 +81,11 @@ public class ActivityMhV3ApiController {
 
     @RequestMapping("activity/brief/info")
     public RestRespDTO briefInfo(@RequestBody String data) {
-        Activity activity = getActivityByData(data);
+        JSONObject params = JSON.parseObject(data);
+        Integer websiteId = params.getInteger("websiteId");
+        // 根据websiteId查询活动id
+        Activity activity = activityQueryService.getByWebsiteId(websiteId);
+        activityQueryService.fillTagNames(Lists.newArrayList(activity));
         MhGeneralAppResultDataDTO mainFields = MhGeneralAppResultDataDTO.buildDefault();
         List<MhGeneralAppResultDataDTO.MhGeneralAppResultDataFieldDTO> fields = Lists.newArrayList();
         mainFields.setFields(fields);
@@ -88,48 +94,118 @@ public class ActivityMhV3ApiController {
             jsonObject.put("results", mainFields);
             return RestRespDTO.success(jsonObject);
         }
-        String signUpKeyword = getSignUpKeyword(activity.getMarketId());
-        Map<String, String> fieldCodeNameMap = activityQueryService.getFieldCodeNameRelation(activity);
+        SignStatDTO signStat = signApiService.getSignParticipation(activity.getSignId());
         // 封面
-        buildField(0, "", "", "", fields);
-        // 活动名称
-        buildField(1, fieldCodeNameMap.getOrDefault("activity_name", "活动名称"), activity.getName(), "", fields);
-        // 子标题
-        buildField(2, "", "", "", fields);
-        // 作者
-        buildField(3, "", "", "", fields);
-        // 简介
-        buildField(4, "", "", "", fields);
+        buildEmptyField(0, fields);
+        // 状态
+        buildStatusValue(activity, signStat, fields);
+        // 标题
+        buildField(3, "", activity.getName(), "", fields);
+        // 标签
+        buildField(4, "", getTagValue(activity), "", fields);
         // 数值
-        buildField(5, "", "", "", fields);
-        // 发布时间
-        buildField(6, "", "", "", fields);
-        // 开始时间
-        buildField(100, fieldCodeNameMap.getOrDefault("activity_time_scope", "活动时间"), DateTimeFormatterConstant.YYYY_MM_DD_HH_MM.format(activity.getStartTime()), "", fields);;
-        // 结束时间
-        buildField(101, fieldCodeNameMap.getOrDefault("activity_time_scope", "活动时间"), DateTimeFormatterConstant.YYYY_MM_DD_HH_MM.format(activity.getEndTime()), "", fields);;
-        // 标签1
-        buildField(102, "", "", "", fields);
-        // 标签2
-        buildField(103, "", "", "", fields);
+        buildEmptyField(5, fields);
+        // 活动时间范围字符串
+        buildField(6, "", getActivityTimeScope(activity), "", fields);
+        // 地点
+        buildAddress(activity, fields);
+        // 报名人数
+        buildSignUpNum(signStat, fields);
+        jsonObject.put("results", Lists.newArrayList(mainFields));
+        return RestRespDTO.success(jsonObject);
+    }
+
+    private void buildStatusValue(Activity activity, SignStatDTO signStat, List<MhGeneralAppResultDataDTO.MhGeneralAppResultDataFieldDTO> fields) {
+        ActivityStatusUtils.StatusEnum status = ActivityStatusUtils.calActivityShowStatus(activity.getStartTime(), activity.getEndTime(), signStat.getSignUpStartTime(), signStat.getSignUpEndTime());
+        String statusValue = status.getName();
+        String statusColorCss = "";
+        switch (status) {
+            case SIGN_UP_ONGOING:
+                statusColorCss = "linear-gradient(270deg, #24BB1D 0%, #48DF4E 100%);";
+                break;
+            case ABOUT_TO_START:
+                statusColorCss = "linear-gradient(96.68deg, rgba(255, 183, 99, 0.9) 1.14%, rgba(255, 139, 32, 0.9) 98.13%);";
+                break;
+            case ONGOING:
+                statusColorCss = "linear-gradient(90deg, #02C4FF 0%, #43A3FF 100%);";
+                break;
+            case ENDED:
+                statusColorCss = "linear-gradient(90deg, #ACB8BF 4.35%, #7F8E98 100%);";
+                break;
+            default:
+
+        }
+        // 状态
+        buildField(1, "", statusValue, "", fields);
+        // 按钮颜色
+        buildField(2, "", statusColorCss, "", fields);
+    }
+
+    private void buildAddress(Activity activity, List<MhGeneralAppResultDataDTO.MhGeneralAppResultDataFieldDTO> fields) {
+        String address = "";
+        String addressUrl = "";
+        BigDecimal longitude = activity.getLongitude();
+        BigDecimal dimension = activity.getDimension();
+        if (longitude != null && dimension != null) {
+            address = Optional.ofNullable(activity.getAddress()).filter(StringUtils::isNotBlank).orElse("");
+            address += Optional.ofNullable(activity.getDetailAddress()).filter(StringUtils::isNotBlank).orElse("");
+            addressUrl = DomainConstant.API + "/redirect/activity/" + activity.getId() + "/address";
+        }
+        buildField(100, "", address, addressUrl, fields);
+    }
+
+    private void buildSignUpNum(SignStatDTO signStat, List<MhGeneralAppResultDataDTO.MhGeneralAppResultDataFieldDTO> fields) {
         // 报名参与情况
         String signedUpNumDescribe = "";
-        if (activity.getSignId() != null) {
-            SignStatDTO signStat = signApiService.getSignParticipation(activity.getSignId());
+        String signUpListUrl = "";
+        if (signStat != null) {
             if (CollectionUtils.isNotEmpty(signStat.getSignUpIds())) {
                 signedUpNumDescribe = String.valueOf(signStat.getSignedUpNum());
                 if (signStat.getLimitNum() != null && signStat.getLimitNum() > 0) {
                     signedUpNumDescribe += "/" + signStat.getLimitNum();
                 }
             }
+            // 开启了报名名单公开则显示报名人数链接
+            Boolean publicList = Optional.ofNullable(signStat.getPublicList()).orElse(false);
+            if (publicList) {
+                signUpListUrl = signApiService.getSignUpListUrl(signStat.getSignUpIds().get(0));
+            }
         }
-
         // 已报名人数
-        buildField(104, "已" + signUpKeyword, signedUpNumDescribe, "", fields);
-        // 报名时间
-        buildField(105, "", "", "", fields);
-        jsonObject.put("results", Lists.newArrayList(mainFields));
-        return RestRespDTO.success(jsonObject);
+        buildField(101, "", signedUpNumDescribe, signUpListUrl, fields);
+    }
+
+    private String getTagValue(Activity activity) {
+        List<String> tagNames = activity.getTagNames();
+        StringBuilder tagValue = new StringBuilder();
+        for (String tagName : tagNames) {
+            tagValue.append("<span class='info-label'>");
+            tagValue.append(tagName);
+            tagValue.append("</span>");
+        }
+        return tagValue.toString();
+    }
+
+    private String getActivityTimeScope(Activity activity) {
+        StringBuilder timeScope = new StringBuilder();
+        LocalDateTime startTime = activity.getStartTime();
+        LocalDateTime endTime = activity.getEndTime();
+        timeScope.append(startTime.format(DateTimeFormatterConstant.YYYY_MM_DD_HH_MM));
+        timeScope.append(" ~ ");
+        if (endTime.toLocalDate().toEpochDay() - startTime.toLocalDate().toEpochDay() == 0) {
+            // 同一天
+            timeScope.append(endTime.format(DateTimeFormatterConstant.HH_MM));
+            // 星期几
+            timeScope.append(" ");
+            timeScope.append(DateUtils.calDayOfWeek(startTime));
+        } else {
+            timeScope.append(endTime.format(DateTimeFormatterConstant.YYYY_MM_DD_HH_MM));
+        }
+        return timeScope.toString();
+    }
+
+    private void buildEmptyField(Integer flag, List<MhGeneralAppResultDataDTO.MhGeneralAppResultDataFieldDTO> fields) {
+        buildField(flag, "", "", "", fields);
     }
 
     /**通用活动门户按钮数据源
@@ -686,11 +762,7 @@ public class ActivityMhV3ApiController {
         return signUpKeyword;
     }
 
-    private void buildField(Integer flag,
-                            String key,
-                            String value,
-                            String osrUrl,
-                            List<MhGeneralAppResultDataDTO.MhGeneralAppResultDataFieldDTO> fields) {
+    private void buildField(Integer flag, String key, String value, String osrUrl, List<MhGeneralAppResultDataDTO.MhGeneralAppResultDataFieldDTO> fields) {
         fields.add(MhGeneralAppResultDataDTO.MhGeneralAppResultDataFieldDTO.builder()
                 .key(key)
                 .flag(String.valueOf(flag))
